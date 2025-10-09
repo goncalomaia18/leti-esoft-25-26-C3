136be13fd43abb25138e7ca0f50e28c8ce97c272
goncalomaia18 <goncalo.maia@outlook.com>
2025-10-09 12:02:26 +0100

    1

---
+++ cmake-build-debug/CoreTests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/gonca/Desktop/project-template/CoreTests/lib/googletest
+# Build directory: C:/Users/gonca/Desktop/project-template/cmake-build-debug/CoreTests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
