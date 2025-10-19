e9de88e3b95737381feee314fe4a3cbdbd22c7fc
David <1241447@isep.ipp.pt>
2025-10-19 18:45:09 +0100

    Algumas alteraçoes

---
+++ cmake-build-debug/Core/CMakeFiles/Core.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Core.dir/sources/domain/shared/Result.cpp.obj"
+  "CMakeFiles/Core.dir/sources/domain/shared/Result.cpp.obj.d"
+  "CMakeFiles/Core.dir/sources/domain/shared/StringUtils.cpp.obj"
+  "CMakeFiles/Core.dir/sources/domain/shared/StringUtils.cpp.obj.d"
+  "libCore.a"
+  "libCore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
